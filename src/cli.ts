f9a8abfa728ac311dfbf291cef1be182b137a2fc
jch1223 <ico1828@gmail.com>
2025-11-12 00:32:10 +0900

    feat: build pipline

---
+++ src/cli.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("create-vitnal")
+  .description(
+    "Bootstrap a Vitnal React + TypeScript project with curated defaults."
+  )
+  .version("0.1.0");
+
+program
+  .command("create")
+  .description("Generate a new Vitnal project in the target directory.")
+  .argument("<project-name>", "The name of the project directory to create")
+  .action((projectName: string) => {
+    // TODO: Replace with scaffold logic (copy template, install deps, etc.)
+    console.log(`Project directory '${projectName}' will be created.`);
+  });
+
+program.parse(process.argv);
