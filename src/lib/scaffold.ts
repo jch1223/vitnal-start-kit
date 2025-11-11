13358114a685b11d5e595fe25de6caedbfb182aa
jch1223 <ico1828@gmail.com>
2025-11-12 06:45:02 +0900

    feat: 템플릿 생성시 예외 처리

--- src/lib/scaffold.ts
+++ src/lib/scaffold.ts
@@ -1,3 +1,4 @@
+import { copyTemplateDirectory, ensureTargetDirectoryAvailable } from './filesystem';
 import { resolveScaffoldPaths } from './paths';
 
 import type { ResolvedConfig } from '../types/config';
@@ -9,11 +10,13 @@ import type { ResolvedConfig } from '../types/config';
 export const createProject = async (config: ResolvedConfig): Promise<void> => {
   const paths = resolveScaffoldPaths(config);
 
-  console.log(`Project directory '${config.projectName}' will be created.`);
-  console.log('Selected options:', config.options);
-  console.log('Resolved paths:', paths);
+  console.log(`타깃 디렉터리: ${paths.targetDir}`);
+  console.log('선택된 옵션:', config.options);
 
-  // TODO: 기본 템플릿을 대상 디렉터리에 복사합니다.
+  await ensureTargetDirectoryAvailable(paths.targetDir);
+  await copyTemplateDirectory(paths.templateDir, paths.targetDir);
+  console.log('기본 템플릿 복사가 완료되었습니다.');
+
   // TODO: 선택한 옵션(Tailwind, Taskmaster 등)을 템플릿 파일에 반영합니다.
   // TODO: 새 프로젝트 디렉터리에서 npm으로 의존성을 설치합니다.
   // TODO: 설정 요약과 안내를 담은 README.md를 생성합니다.
